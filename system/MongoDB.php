f9017fdba95fa145fafb6424c1f24f18ee5dcc2f
Anatoliy Vetrenko <anatoliy.vetrenko@gmail.com>
2020-12-02 20:59:36 +0200

    change config and connection

--- system/MongoDB.php
+++ system/MongoDB.php
@@ -13,17 +13,11 @@ class MongoDB
     {
         if(is_null(self::$config) || is_null(self::$db)){
             self::$config = require_once '../config/mongo_db.php';
-            self::$db = self::connection();
+            self::$db = new Mongo(self::$config['connection_string']);
         }
 
         $name = self::$config['db_name'];
 
         return self::$db->$name;
-    }
-
-    private static function connection()
-    {
-        $options = 'mongodb://'.self::$config['username'].':'.self::$config['password'].'@127.0.0.1:27017';
-        return new Mongo($options);
     }
 }
